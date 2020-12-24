280097df18e1b7bcccad811e6464708e3fa69b21
hunglnl01 <hunglnl01@gmail.com>
2020-12-24 10:41:12 -0800

    practice question

--- Arrays_and_Strings/one1.java
+++ Arrays_and_Strings/one1.java
@@ -1,41 +1,46 @@
-// solution 1
-// time complexity O(n) with n is the length of the string
-// space complexity O(1)
-boolean one1_1(String str){
-    // ASCII alphabet only has 128 character
-    if(str.length() > 128) return false;
+class solution1 {
+    // solution 1
+    // time complexity O(n) with n is the length of the string
+    // space complexity O(1)
+    public boolean one1_1(String str) {
+        // ASCII alphabet only has 128 character
+        if (str.length() > 128)
+            return false;
 
-    boolean[] ascii_char = new boolean[128];
+        boolean[] ascii_char = new boolean[128];
 
-    for(int i = 0; i < str.length(); i++){
-        int val = str.charAt(i);
-        // if seen, return false
-        if(ascii_char[val] == true) return false;
-        // not seen, set to true
-        ascii_char[val] = true;
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            // if seen, return false
+            if (ascii_char[val] == true)
+                return false;
+            // not seen, set to true
+            ascii_char[val] = true;
+        }
+        return true;
     }
-    return true;
-}
 
+    // solution 2
+    // time complexity O(n) with n is the length of the string
+    // space complexity is reduced by a factor of 8 using a single int variable
+    // instead of an array
+    public boolean one1_2(String str) {
+        // checker to check the matching bit
+        // checker in binary = 00000
+        int checker = 0;
 
-// solution 2
-// time complexity O(n) with n is the length of the string
-// space complexity is reduced by a factor of 8 using a single int variable instead of an array
-boolean one1_2(String str){
-    // checker to check the matching bit
-    // checker in binary = 00000
-    int checker = 0;
+        for (int i = 0; i < str.length(); i++) {
+            // assume its from 'a' to 'z'
+            int val = str.charAt(i) - 'a';
 
-    for(int i = 0; i < str.length(); i++){
-        // assume its from 'a' to 'z'
-        int val = str.charAt(i) - 'a';
-
-        // 1<<val = 10000...000
-        // bitwise AND with checker, if the checker and 1<<val has matching index
-        // return false
-        if((checker & (1<<val)) > 0) return false;
-        // not seen, add the new bit to checker
-        checker |= (1<<val);
+            // 1<<val = 10000...000
+            // bitwise AND with checker, if the checker and 1<<val has matching index
+            // return false
+            if ((checker & (1 << val)) > 0)
+                return false;
+            // not seen, add the new bit to checker
+            checker |= (1 << val);
+        }
+        return true;
     }
-    return true;
 }
---
+++ Arrays_and_Strings/one2.java
@@ -0,0 +1,41 @@
+class solution2 {
+    // solution 1
+    // inefficient
+    // sort the strings in order
+    public String sort(String str) {
+        // strings are immutable, convert to array, then sort
+        char[] str_set = str.toCharArray();
+        java.util.Arrays.sort(str_set);
+        return new String(str_set);
+    }
+
+    public boolean permutation_1(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+        return sort(s1).equals(sort(s2));
+    }
+
+    // solution 2
+    public boolean permutation_2(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+
+        // array to store how # of times each character appears
+        int[] letter_set = new int[128];
+
+        // store the # of times each character appears in s1
+        for (char c : s1.toCharArray()) {
+            letter_set[c]++;
+        }
+
+        // iterate throught s2 to check for disparity
+        for (int i = 0; i < s2.length(); i++) {
+            int num = (int) s2.charAt(i);
+            letter_set[num]--;
+            if (letter_set[num] < 0)
+                return false;
+        }
+
+        return true;
+    }
+}
